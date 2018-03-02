46020ced0458f20c796b6d30b9a645c70f4ef4d3
Daniel Gaias Malagurti <daniel_malagurtti@hotmail.com>
2018-03-02 12:43:37 -0300

    Configuração do Cors
    Confirguração de liberação do Cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,54 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 const app = express();
+
+app.use(cors());
+
+app.get('/barraca', function (req, res, next) {
+    res.json({msg: 'Cors Ok, Todas as origens'})
+  });
+
+app.post('/barraca', function (req, res, next) {
+    res.json({msg: 'Cors Ok, Todas as origens'})
+  });
+
+app.put('/barraca/barracaId', function (req, res, next) {
+    res.json({msg: 'Cors Ok, Todas as origens'})
+  });
+
+app.delete('/barraca/barracaId', function (req, res, next) {
+    res.json({msg: 'Cors Ok, Todas as origens'})
+  });
+
+app.get('/barraca/barracaId', function (req, res, next) {
+    res.json({msg: 'Cors Ok, Todas as origens'})
+  });
+
+app.post('/auth/autenticacao', function (req, res, next) {
+    res.json({msg: 'Cors Ok, Todas as origens'})
+  });
+
+app.post('/auth/registro', function (req, res, next) {
+    res.json({msg: 'Cors Ok, Todas as origens'})
+  });
+
+app.post('/auth/autenticacao', function (req, res, next) {
+    res.json({msg: 'Cors Ok, Todas as origens'})
+  });
+
+app.post('/auth/resetsenha', function (req, res, next) {
+    res.json({msg: 'Cors Ok, Todas as origens'})
+  });
+
+app.post('/auth/recuperasenha', function (req, res, next) {
+    res.json({msg: 'Cors Ok, Todas as origens'})
+  });
+
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
